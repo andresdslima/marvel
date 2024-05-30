6a0bb3b59e2ba051286cdce429a4d51f9d1324fa
Andre Lima <andre.lima@innowave.tech>
2024-05-30 19:47:18 +0100

    refactor: mock navigation + folder structure

--- App.jsx
+++ App.jsx
@@ -1,16 +1,43 @@
-import { NavigationContainer } from '@react-navigation/native';
-import LoginScreen from './src/Screens/LoginScreen';
-import WelcomeScreen from './src/Screens/WelcomeScreen';
-import HeroesScreen from './src/Screens/HeroesScreen';
-import HeroeDetailsScreen from './src/Screens/HeroeDetailsScreen';
+import { useState, useContext } from "react";
+import LoginScreen from "./screens/LoginScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import HeroesScreen from "./screens/HeroesScreen";
+import HeroeDetailsScreen from "./screens/HeroeDetailsScreen";
 
 export default function App() {
+  const [mockNavigation, setMockNavigation] = useState(0);
+  // Could create a context to simulate transfering data via navigation
+  // from HeroesScreen to HeroDetailsScreen (when a specific hero is selected)
+
+  const navigateForward = () => {
+    if (mockNavigation < 3) {
+      setMockNavigation((prevState) => prevState + 1);
+    }
+  };
+
+  const navigateBackward = () => {
+    if (mockNavigation > 0) {
+      setMockNavigation((prevState) => prevState + -1);
+    }
+  };
+
   return (
-    <NavigationContainer>
-      {/* <LoginScreen /> */}
-      {/* <WelcomeScreen /> */}
-      <HeroesScreen />
-      {/* <HeroeDetailsScreen /> */}
-    </NavigationContainer>
+    <>
+      {mockNavigation === 0 && (
+        <LoginScreen navigateForward={navigateForward} />
+      )}
+      {mockNavigation === 1 && (
+        <WelcomeScreen navigateForward={navigateForward} />
+      )}
+      {mockNavigation === 2 && (
+        <HeroesScreen
+          navigateForward={navigateForward}
+          navigateBackward={navigateBackward}
+        />
+      )}
+      {mockNavigation === 3 && (
+        <HeroeDetailsScreen navigateBackward={navigateBackward} />
+      )}
+    </>
   );
 }
